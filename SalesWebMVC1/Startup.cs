b9e816710a3cf8c8fd8a33898295920660eb984d
Fernanda Gomes <fernandaggarcia@hotmail.com>
2020-02-26 11:57:48 -0300

    Implementing simple search

--- SalesWebMVC1/Startup.cs
+++ SalesWebMVC1/Startup.cs
@@ -48,6 +48,7 @@ namespace SalesWebMVC1
             services.AddScoped<SeedingService>();
             services.AddScoped<SellerService>();
             services.AddScoped<DepartmentService>();
+            services.AddScoped < SalesRecordService>();
 
         }   
 
